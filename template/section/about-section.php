a50e385ed684349d97e8c8b55b94869a5c247f09
xafar_Desktop <xafar.ali.malk@gmail.com>
2025-10-23 18:33:02 +0500

    pages client added

---
+++ template/section/about-section.php
@@ -0,0 +1,23 @@
+<?php
+    global $images;
+?>
+
+<div class="auto-scroll-wrapper" id="sec-about-home">
+        <div class="section lg auto-scroll mc fs">
+            <div class="container  wow fadeInUpBig">
+                <div class="row">
+                    <div class="col-md-6">
+                        <h2 class="display-2 text-uppercase h1 text-right font-thin">From Insight to <span class="highlight h1"><strong>Impact</strong></span></h2>
+                
+                        <div class="spacer"></div>                           
+                        </div>
+
+                        <div class="col-md-5 offset-md-7">
+                            <p class="lead ">We believe technology should tell stories — your data, your processes, your future. <br><strong>At TnZ International</strong>, we transform complexity into clarity.</p>
+                        </div>
+                </div>
+                
+                
+            </div>
+        </div>
+    </div>
---
+++ template/section/client-section.php
@@ -0,0 +1,42 @@
+<?php
+global $images;
+?>
+
+<div class="auto-scroll-wrapper" id="sec-client-home">
+        <div class="bg-illus">
+            <img src="/images/illus-home-client.svg" alt="">
+            <div class="tag has-icon">
+                <span class="fa fa-star badge"></span>
+                Parnter with Giant Globals
+            </div>
+        </div>
+        <div class="section lg auto-scroll mc fs">
+            <div class="container  wow fadeInUpBig">
+
+                <div class="row">                   
+                     <div class="col-md-8 text-center offset-md-2">
+                        <p class="lead xl">
+                            Trusted by leading companies in various industries. Small to enterprise.
+                        </p>
+                     </div>
+                </div>               
+                
+            </div>
+
+            <div class="container-fluid">
+                <div class="glossy client-list">
+                    <div class="client-logo-wrapper">
+                        <img src="/images/logo-claude.svg" alt="">
+                        <img src="/images/logo-firebase.svg" alt="">
+                        <img src="/images/logo-gitlab.svg" alt="">
+                        <img src="/images/logo-importio.svg" alt="">
+                        <img src="/images/logo-azure.svg" alt="">
+                        <img src="/images/logo-firebase.svg" alt="">
+                        <img src="/images/logo-gitlab.svg" alt="">
+                        <img src="/images/logo-importio.svg" alt="">
+                        <img src="/images/logo-azure.svg" alt="">
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
